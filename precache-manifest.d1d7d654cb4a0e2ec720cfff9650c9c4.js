11f7dfb48b0548a2b50747ac8877eacd3664566d
Salman <salman.samian@gmail.com>
2020-08-02 01:12:00 +0430

    first commit

---
+++ precache-manifest.d1d7d654cb4a0e2ec720cfff9650c9c4.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad666daaa7ec3ec38977515e64221cb5",
+    "url": "/snappclub/index.html"
+  },
+  {
+    "revision": "f5ec1e8f06e0fc5441cc",
+    "url": "/snappclub/static/css/3.3fd67f12.chunk.css"
+  },
+  {
+    "revision": "52dd702062772631e489",
+    "url": "/snappclub/static/css/main.92cce91e.chunk.css"
+  },
+  {
+    "revision": "f5ec1e8f06e0fc5441cc",
+    "url": "/snappclub/static/js/3.71991d5a.chunk.js"
+  },
+  {
+    "revision": "cb0ee57be6b74499a40473be37de0759",
+    "url": "/snappclub/static/js/3.71991d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737f9dffa35251f49d04",
+    "url": "/snappclub/static/js/4.ab894449.chunk.js"
+  },
+  {
+    "revision": "05800c36d50b10493495",
+    "url": "/snappclub/static/js/5.9fc5d0ad.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/snappclub/static/js/5.9fc5d0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7feb26e71c5295e775a",
+    "url": "/snappclub/static/js/6.f50b517a.chunk.js"
+  },
+  {
+    "revision": "96c73944233a509a3aab13867e8f94e4",
+    "url": "/snappclub/static/js/6.f50b517a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1477ab90d3cde92bca7e",
+    "url": "/snappclub/static/js/7.98364f98.chunk.js"
+  },
+  {
+    "revision": "5ae66082d956e3bee8e1",
+    "url": "/snappclub/static/js/8.6f0ed065.chunk.js"
+  },
+  {
+    "revision": "2775d006da628a08008d",
+    "url": "/snappclub/static/js/9.18d5bab9.chunk.js"
+  },
+  {
+    "revision": "52dd702062772631e489",
+    "url": "/snappclub/static/js/main.126d78f2.chunk.js"
+  },
+  {
+    "revision": "03662bbc94f43a5d27d4",
+    "url": "/snappclub/static/js/runtime-main.ccef3a38.js"
+  },
+  {
+    "revision": "6475a6443933f9255954",
+    "url": "/snappclub/static/js/xlsx.120876cb.chunk.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/snappclub/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/snappclub/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/snappclub/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "190ad0ed18269b3214ff95ef877dea3b",
+    "url": "/snappclub/static/media/Vazir-Bold-FD.190ad0ed.woff"
+  },
+  {
+    "revision": "5c5b20cfe28a0cbcef5098d649402502",
+    "url": "/snappclub/static/media/Vazir-Bold-FD.5c5b20cf.ttf"
+  },
+  {
+    "revision": "a96779a25d003251be6a4ce61a9d48c8",
+    "url": "/snappclub/static/media/Vazir-Bold-FD.a96779a2.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/snappclub/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/snappclub/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/snappclub/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/snappclub/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/snappclub/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/snappclub/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/snappclub/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/snappclub/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/snappclub/static/media/sprite-flags-24x24.7ed12ca4.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/snappclub/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/snappclub/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/snappclub/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/snappclub/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/snappclub/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
---
+++ static/js/xlsx.120876cb.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpSnappClub=this.webpackJsonpSnappClub||[]).push([[2],{1211:function(n,p){},1291:function(n,p){},1292:function(n,p){}}]);
+//# sourceMappingURL=xlsx.120876cb.chunk.js.map
